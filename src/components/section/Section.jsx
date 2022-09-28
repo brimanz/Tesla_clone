335d60b8e57692bfef7139ffcc1e7970a6b9e26e
jonathan briceño <brimanz2014@gmail.com>
2022-09-27 20:30:23 -0400

    images and some animations

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -5,7 +5,22 @@ import styled from 'styled-components';
 const Section = () =>{
 	return(
 		<Wrap>
-			
+			<ItemText>
+
+				<h1>Model S</h1>
+
+				<p>Order Online for Touchless Delivery</p>
+			</ItemText>
+
+			<Buttons>
+				<ButtonGroup>
+					<LeftButton>Custom Order</LeftButton>
+
+					<RightButton>Existing Inventory</RightButton>
+				</ButtonGroup>
+
+				<DownArrow src="/images/down-arrow.svg"/>
+			</Buttons>
 		</Wrap>
 	);
 }
@@ -17,7 +32,63 @@ export default Section;
 const Wrap = styled.div`
 	width: 100vw;
 	height: 100vh;
+	
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+	background-image: url("/images/model-s.jpg");
 
-	background-image: url('./assets/model-s.jpg');
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
 `
+
+const ItemText = styled.div`
+	padding-top: 15vh;
+	text-align: center;
+`
+
+const ButtonGroup = styled.div`
+	display: flex;	
+	margin-bottom: 3.5rem;
+`
+
+const LeftButton = styled.div`
+	width: 25.6rem;
+	height: 4rem;
+	
+	background-color: var(--color-grey);		
+	color: var(--color-white);
+	
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 10rem;
+	opacity: .8;
+	text-transform: uppercase;
+	font-size: 1.1rem;	
+	cursor: pointer;
+	margin: .8rem;
+`
+
+const RightButton = styled(LeftButton)`
+
+	
+`
+
+const DownArrow = styled.img`
+	margin-top: 2rem;
+	height: 4rem;
+	
+	animation: animatedDown infinite 1.3s;
+`
+
+const Buttons = styled.div`
+	
+`
+
+
+
+
 
